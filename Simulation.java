5cfb8accadad28c3041a6d9d6df5d5ee0cc71b7b
Holly Metzger <hollymmetzger@gmail.com>
2025-04-15 18:51:19 -0400

    added bus stop array and generator

--- Simulation.java
+++ Simulation.java
@@ -1,7 +1,7 @@
 public class Simulation {
     // Parameters for Simulation
     int NUMBEROFPEOPLE, NUMBEROFBUSES, NUMBEROFTRAINS;
-    double TIMEBETWEENTRAINS, DISTANCEBETWEENBUSSTOPS;
+    double TIMEBETWEENTRAINS, DISTANCEBETWEENBUSSTOPS, FREDERICKRADIUS;
     int BUSSPEED, TRAINSPEED;
     double MAXTIMEONBUS, MAXTIMEWAITINGFORBUS;
 
@@ -10,6 +10,7 @@ public class Simulation {
     private People people;
     private Buses buses;
     private Trains trains;
+    private Location[] busStops;
 
     // Fields used while running simulation
     private double currentTime = 0;
@@ -20,7 +21,7 @@ public class Simulation {
     // Constructor
     public Simulation(
             int numberOfPeople, int numberOfBuses, int numberOfTrains,
-            double timeBetweenTrains, double distanceBetweenBusStops,
+            double timeBetweenTrains, double distanceBetweenBusStops, double frederickRadius,
             int busSpeed, int trainSpeed,
             double maxTimeOnBus, double maxTimeWaitingForBus
     ) {
@@ -30,15 +31,17 @@ public class Simulation {
         NUMBEROFTRAINS = numberOfTrains;
         TIMEBETWEENTRAINS = timeBetweenTrains;
         DISTANCEBETWEENBUSSTOPS = distanceBetweenBusStops;
+        FREDERICKRADIUS = frederickRadius;
         BUSSPEED = busSpeed;
         TRAINSPEED = trainSpeed;
         MAXTIMEONBUS = maxTimeOnBus;
         MAXTIMEWAITINGFORBUS = maxTimeWaitingForBus;
 
-        // Declare object classes
+        // Initialize object classes
         people = new People(numberOfPeople);
         buses = new Buses(numberOfBuses);
         trains = new  Trains(numberOfTrains);
+        generateBusStops(FREDERICKRADIUS, DISTANCEBETWEENBUSSTOPS);
 
         // Initialize tracking fields
         currentTime = 0;
@@ -49,10 +52,30 @@ public class Simulation {
     // Advance the simulation by dt, and return the time until next event after that
     double update(double currentTime, double dt) {
         currentTime += dt;
-        return Math.min(
+        double timeUntilNextEvent = Math.min(
             people.update(currentTime, dt),
             buses.update(),
             trains.update()
         );
+
+        if (finishedPeople.length == NUMBEROFPEOPLE) {
+            isFinished = true;
+        }
+        return timeUntilNextEvent;
+    }
+
+
+    // Private methods
+
+    private void generateBusStops(double radius, double distance) {
+        int i = 0;
+        for (double x = -radius; x <= radius; x += distance) {
+            for (double y = -radius; y <= radius; y += distance) {
+                if (x * x + y * y <= radius * radius) {
+                    this.busStops[i] = new Location(x, y);
+                    i++;
+                }
+            }
+        }
     }
 }
