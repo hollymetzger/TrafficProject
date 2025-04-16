016a412a2a3d08c8d9171a92c0cd8d203884011f
Holly Metzger <hollymmetzger@gmail.com>
2025-04-15 20:49:50 -0400

    added bus stop class basics

---
+++ BusStops.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class BusStops {
+
+    // Private fields
+    private ArrayList<Location> busStops;
+
+    // Constructor
+    public BusStops(double radius, double distance) {
+        busStops = new ArrayList<Location>();
+        generateBusStops(radius, distance);
+    }
+
+    // Public methods
+
+    public BusStop getNearestBusStop(Location loc) {
+
+    }
+
+    // Private methods
+    private void generateBusStops(double radius, double distance) {
+        for (double x = -radius; x <= radius; x += distance) {
+            for (double y = -radius; y <= radius; y += distance) {
+                if (x * x + y * y <= radius * radius) {
+                    busStops.add(new Location(x, y));
+                }
+            }
+        }
+    }
+}
