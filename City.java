7191ee2530544c46b8cbd456bc0ab024c1516480
Holly Metzger <hollymmetzger@gmail.com>
2025-04-19 11:02:37 -0400

    added city class

---
+++ City.java
@@ -0,0 +1,22 @@
+public class City extends Location {
+    private String name;
+    private int population;
+    private double radius;
+
+    // Constructor
+    public City (double x, double y, String nm, int pop, double r) {
+        super(x, y);
+        name = nm;
+        population = pop;
+        radius = r;
+    }
+
+    // Public Methods
+
+
+    // Private Methods
+
+
+
+    // Unit Testing Method
+}
