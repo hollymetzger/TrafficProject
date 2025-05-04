88840d343b3598a63f041008fade51db71016c9d
hollymetzger <124737388+hollymetzger@users.noreply.github.com>
2025-05-04 12:27:13 -0400

    improved train setNextStop logic

--- Train.java
+++ Train.java
@@ -5,12 +5,17 @@ public class Train extends Vehicle {
     private double totalDistanceTraveled;
     private Stop[] stops;
     private int nextStop;
+    private boolean southbound;
 
     // Constructor
     public Train(double speed, int maxCapacity) {
         super(speed, maxCapacity);
     }
 
+    // Accessors
+    public boolean isSouthbound() {
+        return southbound;
+    }
 
     // Public methods
     // Advances the train in the simulation by dt time
@@ -39,7 +44,17 @@ public class Train extends Vehicle {
 
     private void setNextStop() {
         int tmp = nextStop;
-        nextStop = (nextStop + 1) % stops.length;
+
+        nextStop += southbound ? 1 : -1;
+        if (nextStop == -1 || nextStop == stops.length) {
+            reverseDirection();
+            nextStop += southbound ? -2 : 2;
+        }
+
         distanceToNextStop = stops[tmp].getDistance(stops[nextStop]);
+    }
+
+    private void reverseDirection() {
+        southbound = !southbound;
     }
 }
